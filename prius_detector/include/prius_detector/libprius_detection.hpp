982684176ee48cf7c4799b8306cff12aee4d3b4f
kpiyush25 <piyushk@iitk.ac.in>
2022-03-15 19:01:12 +0530

    Add basic setup for prius detector

---
+++ prius_detector/include/prius_detector/libprius_detection.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <utility>
+#include <vector>
+
+namespace interiit22::prius_detection {
+
+class PriusDetector {
+  public:
+    std::pair<int, int> getCentre();
+    cv::Mat getThresh();
+    cv::Point2f* getCorners();
+    double getDistance();
+    double getArea();
+
+    void setHSVMin(const int& h, const int& s, const int& v);
+    void setHSVMax(const int& h, const int& s, const int& v);
+    void setMinArea(const int& area);
+    void setCannyParams(const int& lower, const int& upper, const int& size);
+
+    void thresholdImage(cv::Mat& img);
+    void findGoodContours();
+    void drawContours(cv::Mat& img);
+    void findFrameCentre(cv::Mat& img);
+    void fitRect(cv::Mat& img);
+
+    static constexpr double scale_factor = 20160;  // heuristically determined
+
+  private:
+    std::pair<int, int> centre_;
+
+    cv::Scalar hsv_min_;
+    cv::Scalar hsv_max_;
+
+    cv::Mat thresh_img_;
+
+    cv::Point2f center_;
+    cv::Point2f rect_points[4];
+
+    std::vector<std::vector<cv::Point>> good_contours_;
+
+    int canny_param_lower_;
+    int canny_param_upper_;
+    int canny_kernel_size_;
+
+    double min_contour_area_;
+    double distance_;
+    double area_;
+};
+
+}  // namespace interiit22::prius_detection
---
+++ prius_detector/src/prius_detector.cpp
@@ -0,0 +1,88 @@
+#include <prius_detector/prius_detector.hpp>
+
+namespace interiit22::prius_detection {
+
+void PriusDetectorNode::init(ros::NodeHandle& nh) {
+    int h_min, s_min, v_min;
+    int h_max, s_max, v_max;
+    int canny_lower, canny_upper, canny_ker;
+    int min_contour_area;
+
+    img_sub_ = nh.subscribe("image_raw", 1, &PriusDetectorNode::imageCallback, this);
+
+    ros::NodeHandle nh_private("~");
+
+    nh_private.getParam("h_min", h_min);
+    nh_private.getParam("s_min", s_min);
+    nh_private.getParam("v_min", v_min);
+    nh_private.getParam("h_max", h_max);
+    nh_private.getParam("s_max", s_max);
+    nh_private.getParam("v_max", v_max);
+    nh_private.getParam("canny_lower", canny_lower);
+    nh_private.getParam("canny_upper", canny_upper);
+    nh_private.getParam("canny_ker", canny_ker);
+    nh_private.getParam("min_contour_area", min_contour_area);
+
+    detect_.setHSVMin(h_min, s_min, v_min);
+    detect_.setHSVMax(h_max, s_max, v_max);
+    detect_.setCannyParams(canny_lower, canny_upper, canny_ker);
+    detect_.setMinArea(min_contour_area);
+
+    centre_pub_ = nh_private.advertise<detector_msgs::Centre>("centre_coord", 10);
+    thresh_pub_ = nh_private.advertise<sensor_msgs::Image>("thresh_img", 10);
+    contour_pub_ = nh_private.advertise<sensor_msgs::Image>("contours", 10);
+    corners_pub_ = nh_private.advertise<detector_msgs::Corners>("corners", 10);
+}
+
+void PriusDetectorNode::run() {
+    if (img_.empty()) {
+        return;
+    }
+
+    detect_.thresholdImage(img_);
+    detect_.findGoodContours();
+    detect_.drawContours(img_);
+    detect_.fitRect(img_);
+
+    std::pair<int, int> centre_pair = detect_.getCentre();
+    double distance = detect_.getDistance();
+    double area = detect_.getArea();
+    centre_coord_.x = centre_pair.first;
+    centre_coord_.y = centre_pair.second;
+    centre_coord_.d = (float) distance;
+    centre_coord_.a = (float) area;
+    centre_coord_.header.stamp = ros::Time::now();
+
+    cv::Point2f* corners = detect_.getCorners();
+    rect_corners_.c1_x = corners[0].x;
+    rect_corners_.c1_y = corners[0].y;
+    rect_corners_.c2_x = corners[1].x;
+    rect_corners_.c2_y = corners[1].y;
+    rect_corners_.c3_x = corners[2].x;
+    rect_corners_.c3_y = corners[2].y;
+    rect_corners_.c4_x = corners[3].x;
+    rect_corners_.c4_y = corners[3].y;
+
+    sensor_msgs::ImagePtr thresh_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", detect_.getThresh()).toImageMsg();
+    sensor_msgs::ImagePtr contour_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img_).toImageMsg();
+
+    thresh_pub_.publish(thresh_msg);
+    contour_pub_.publish(contour_msg);
+    centre_pub_.publish(centre_coord_);
+    corners_pub_.publish(rect_corners_);
+}
+
+void PriusDetectorNode::imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+    cv_bridge::CvImagePtr cv_ptr_;
+
+    try {
+        cv_ptr_ = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    } catch (cv_bridge::Exception& e) {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    img_ = cv_ptr_->image;
+}
+
+}  // namespace interiit22::prius_detection
