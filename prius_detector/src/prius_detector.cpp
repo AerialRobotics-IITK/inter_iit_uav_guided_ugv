b807ee3176388ba58d6f1b262d277f08bea171a3
varunk122 <varunk@iitk.ac.in>
2022-03-16 20:33:13 +0530

    Fix depth image error using cv_bridge

--- prius_detector/src/prius_detector.cpp
+++ prius_detector/src/prius_detector.cpp
@@ -39,49 +39,76 @@ void PriusDetectorNode::run() {
         return;
     }
 
-    detect_.thresholdImage(img_);
-    detect_.findGoodContours();
-    detect_.drawContours(img_);
-    detect_.fitRect(img_);
+    // detect_.thresholdImage(img_);
+    // detect_.findGoodContours();
+    // detect_.drawContours(img_);
+    // detect_.fitRect(img_);
 
-    std::pair<int, int> centre_pair = detect_.getCentre();
-    double distance = detect_.getDistance();
-    double area = detect_.getArea();
-    centre_coord_.x = centre_pair.first;
-    centre_coord_.y = centre_pair.second;
-    centre_coord_.d = (float) distance;
-    centre_coord_.a = (float) area;
-    centre_coord_.header.stamp = ros::Time::now();
+    // std::pair<int, int> centre_pair = detect_.getCentre();
+    // double distance = detect_.getDistance();
+    // double area = detect_.getArea();
+    // centre_coord_.x = centre_pair.first;
+    // centre_coord_.y = centre_pair.second;
+    // centre_coord_.d = (float) distance;
+    // centre_coord_.a = (float) area;
+    // centre_coord_.header.stamp = ros::Time::now();
 
-    cv::Point2f* corners = detect_.getCorners();
-    rect_corners_.c1_x = corners[0].x;
-    rect_corners_.c1_y = corners[0].y;
-    rect_corners_.c2_x = corners[1].x;
-    rect_corners_.c2_y = corners[1].y;
-    rect_corners_.c3_x = corners[2].x;
-    rect_corners_.c3_y = corners[2].y;
-    rect_corners_.c4_x = corners[3].x;
-    rect_corners_.c4_y = corners[3].y;
+    // cv::Point2f* corners = detect_.getCorners();
+    // rect_corners_.c1_x = corners[0].x;
+    // rect_corners_.c1_y = corners[0].y;
+    // rect_corners_.c2_x = corners[1].x;
+    // rect_corners_.c2_y = corners[1].y;
+    // rect_corners_.c3_x = corners[2].x;
+    // rect_corners_.c3_y = corners[2].y;
+    // rect_corners_.c4_x = corners[3].x;
+    // rect_corners_.c4_y = corners[3].y;
 
-    sensor_msgs::ImagePtr thresh_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", detect_.getThresh()).toImageMsg();
-    sensor_msgs::ImagePtr contour_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img_).toImageMsg();
+    // sensor_msgs::ImagePtr thresh_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", detect_.getThresh()).toImageMsg();
+    sensor_msgs::ImagePtr contour_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", img_).toImageMsg();
 
-    thresh_pub_.publish(thresh_msg);
+    // thresh_pub_.publish(thresh_msg);
     contour_pub_.publish(contour_msg);
-    centre_pub_.publish(centre_coord_);
-    corners_pub_.publish(rect_corners_);
+    // centre_pub_.publish(centre_coord_);
+    // corners_pub_.publish(rect_corners_);
 }
 
 void PriusDetectorNode::imageCallback(const sensor_msgs::ImageConstPtr& msg) {
     
-    uint8_t img[msg->height][msg->width];
-    for(int i = 0; i <msg->height; i++) {
-        for(int j = 0; j <msg->width; j++) {
-            img[i][j] = (int)msg->data[j + msg->width*i];
-        }
+    // int h = msg->height;
+    // int w = msg->width*4;
+    // uint8_t img[h][w];
+    // for(int i = 0; i <h; i++) {
+    //     for(int j = 0; j <w; j++) {
+    //         img[i][j] = (int)msg->data[j + w*i];
+    //     }
+    // }
+    // cv::Mat src = cv::Mat(h,w/4,CV_8UC1,&img);
+    cv_bridge::CvImagePtr cv_ptr;
+    //Convert from the ROS image message to a CvImage suitable for working with OpenCV for processing
+    try
+    {
+        //Always copy, returning a mutable CvImage
+        //OpenCV expects color images to use BGR channel order.
+        cv_ptr = cv_bridge::toCvCopy(msg);
     }
-    cv::Mat src = cv::Mat(msg->height,msg->width,CV_8U,&img);
-    img_ = src;
+    catch (cv_bridge::Exception& e)
+    {
+        //if there is an error during conversion, display it
+        ROS_ERROR("tutorialROSOpenCV::main.cpp::cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    //Copy the image.data to imageBuf.
+    img_ = cv_ptr->image;
+    cv::namedWindow("img_1");
+    cv::imshow("img_1", img_);
+    cv::convertScaleAbs(img_, img_, 100, 0.0);
+    // img_.convertTo(img_, CV_8UC1);
+    // depthToCV8UC1(depth_float_img, depth_mono8_img);
+    cv::namedWindow("img");
+    cv::imshow("img", img_);
+    cv::waitKey(3);
+    // img_ = src;
 }
 
 }  // namespace interiit22::prius_detection
