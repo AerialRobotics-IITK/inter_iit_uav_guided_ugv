bdae02a84659afd34d7de72829e5209c7426eb85
vakshit <akshitv18@gmail.com>
2022-03-16 18:49:55 +0530

    add test file from csv

--- visualization/src/visualization_node.cpp
+++ visualization/src/visualization_node.cpp
@@ -1,28 +1,40 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
-
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
 #include <cmath>
 
-void mean_path( visualization_msgs::Marker& points , visualization_msgs::Marker& line_strip ) {
-
-    float f = 0.0;
-    for (uint32_t i = 0; i < 100; ++i)
+void mean_path(visualization_msgs::Marker &points, visualization_msgs::Marker &line_strip)
+{
+  std::vector<std::vector<std::string>> content;
+  std::vector<std::string> row;
+  std::string line, word;
+  std::fstream file(("/home/akshit/ros/drdo22_ws/src/inter_iit_uav_guided_ugv/prius_controller/testing/mean_path/world2/world2.csv"), std::ios::in);
+  float f = 0.0;
+  geometry_msgs::Point p;
+  if (file.is_open())
+  {
+    while (getline(file, line))
     {
-      float y = 5 * sin(f + i / 100.0f * 2 * M_PI);
-      float z = 5 * cos(f + i / 100.0f * 2 * M_PI);
+      row.clear();
 
-      geometry_msgs::Point p;
-      p.x = (int32_t)i - 50;
-      p.y = y;
-      p.z = z;
+      std::stringstream str(line);
 
+      while (getline(str, word, ','))
+        row.push_back(word);
+      p.x = std::stod(row[0]);
+      p.y = std::stod(row[1]);
+      p.z = std::stod(row[2]);
       points.points.push_back(p);
       line_strip.points.push_back(p);
+      // content.push_back(row);
     }
-
+  }
 }
 
-int main( int argc, char** argv )
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "trajectory_visualization");
   ros::NodeHandle n;
@@ -35,7 +47,7 @@ int main( int argc, char** argv )
   points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "/base_link";
   points.header.stamp = line_strip.header.stamp = ros::Time::now();
   points.ns = line_strip.ns = "trajectory";
-  points.action = line_strip.action =  visualization_msgs::Marker::ADD;
+  points.action = line_strip.action = visualization_msgs::Marker::ADD;
   points.pose.orientation.w = line_strip.pose.orientation.w = 1.0;
 
   points.id = 0;
@@ -49,14 +61,12 @@ int main( int argc, char** argv )
 
   line_strip.scale.x = 0.1;
 
-
   points.color.g = 1.0f;
   points.color.a = 1.0;
 
   // Line strip is blue
   line_strip.color.b = 1.0;
   line_strip.color.a = 1.0;
-
 
   mean_path(points, line_strip);
 
@@ -66,6 +76,6 @@ int main( int argc, char** argv )
     marker_pub.publish(points);
     marker_pub.publish(line_strip);
 
-    r.sleep();     
+    r.sleep();
   }
 }
