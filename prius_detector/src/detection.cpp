2a152045665c558401cbce8af225e6233b1727c8
AdiPratapSingh <apsinghnsa@gmail.com>
2022-03-18 19:07:00 +0530

    got_depth

--- prius_detector/src/detection.cpp
+++ prius_detector/src/detection.cpp
@@ -4,7 +4,7 @@
 #include <image_transport/image_transport.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
-
+#include <bits/stdc++.h>
 #include <iostream>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -13,11 +13,47 @@
 
 
 cv::Mat img_;
+cv::Mat depth(img_.size(), img_.type());
 
-void imageProcessing() {
+std::string type2str(int type) {
+  std::string r;
 
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch ( depth ) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+  }
+
+  r += "C";
+  r += (chans+'0');
+
+  return r;
+}
+
+cv::Mat GetPixelsFromMat( const cv::Mat& I, const std::vector<cv::Point2f>& points )
+{
+    // some pre-condition:
+    cv::Mat res( 1, (int)points.size( ), CV_8UC1 );
+
+    int i = 0;
+    for( const auto& point : points )
+        res.ptr( 0 )[ i++ ] = I.at<uchar>( cvRound( point.y ), cvRound( point.x ) );
+
+    return res;
+}
+
+void imageProcessing(cv::Mat &depth) {
+// void imageProcessing() {
 	cv::Mat mask;
-    cv::threshold(img_, mask, 0, 255, CV_THRESH_BINARY_INV | CV_THRESH_OTSU);
+    cv::threshold(img_, mask, 0, 60, CV_THRESH_BINARY_INV | CV_THRESH_OTSU);
 
     //! Finding contours..
     std::vector<std::vector<cv::Point>> contours;
@@ -31,19 +67,21 @@ void imageProcessing() {
     // cv::imshow("second draw drawing", drawing);
 
 
+    cv::Scalar color = cv::Scalar(255, 255, 255);
+    drawContours( drawing, contours, -1, color, 1, 8, hierarchy, 1, cv::Point() );
     for (int i = 0; i< contours.size(); i++){
-        cv::Scalar color = cv::Scalar(0, 100, 0);
-        drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, cv::Point() );
-
+        // std::cout<<"here";
         float ctArea= cv::contourArea(contours[i]);
+        std::cout<<"Area of countour : "<<ctArea<<"\n";
         if (ctArea > biggestContourArea){
             biggestContourArea = ctArea;
             biggestContourIdx = i;
         }
     }
+    std::cout<<"--------------------------------\n";
 
     //! If no contour found
-    if(biggestContourIdx < 0){
+    if(contours.size() == 0){
         std::cout << "No contour found..\n";
         return;
     }
@@ -55,11 +93,13 @@ void imageProcessing() {
 
     //! Draw the rotated rect
     cv::Point2f corners[4];
+
+    // Extracting corners of the bounding box
     boundingBox.points(corners);
-    cv::line(drawing, corners[0], corners[1], cv::Scalar(255,255,255));
-    cv::line(drawing, corners[1], corners[2], cv::Scalar(255,255,255));
-    cv::line(drawing, corners[2], corners[3], cv::Scalar(255,255,255));
-    cv::line(drawing, corners[3], corners[0], cv::Scalar(255,255,255));
+    cv::line(mask, corners[0], corners[1], cv::Scalar(255,255,255));
+    cv::line(mask, corners[1], corners[2], cv::Scalar(255,255,255));
+    cv::line(mask, corners[2], corners[3], cv::Scalar(255,255,255));
+    cv::line(mask, corners[3], corners[0], cv::Scalar(255,255,255));
     
     //! Draw center point..
     cv::Point2f center;
@@ -69,13 +109,17 @@ void imageProcessing() {
     } 
     center.x /= 4;
     center.y /= 4;
-    cv::circle(drawing, center, 2, cv::Scalar(255,255,255), 2);
+    std::cout<<"x : "<<center.x<<" y : "<<center.y<<"\n";
+    std::cout<<"depth : "<<depth.at<float>(center.x,center.y)<<"\n";
+    
+    cv::circle(mask, center, 2, cv::Scalar(255,255,255), 2);
 
     // ! Display
-    cv::imshow("Original", img_);
+    cv::imshow("Original", mask);
     cv::imshow("Center Detection", drawing);
     cv::waitKey(1);
-    // cv::destroyAllWindows();
+
+    // cv::destroyAllWindows(); 
     return;
 }
 
@@ -97,14 +141,23 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
 
     // Copy the image.data to imageBuf.
     img_ = cv_ptr->image;
-    std::cout<<img_<<std::endl;
     // cv::Mat temp_img = cv_ptr->image;
     // cv::cvtColor(temp_img, img_, cv::COLOR_GRAY2RGB);
+    // std::cout<<"img_"<<img_<<"\n";
+
+    // !img_ is Matrix: 32FC1 640x480 
+    img_.copyTo(depth);
+    // std::cout<<"depth : "<<img_.at<float>(center.x,center.y)<<"\n";
+
+    // std::cout<<"depth : "<<depth<<std::endl;
     cv::normalize(img_, img_, 0, 255, cv::NORM_MINMAX);
     cv::convertScaleAbs(img_, img_, 1, 0.0);
 	cv::namedWindow("img_");
     cv::imshow("img_", img_);
 	cv::waitKey(1);
+    imageProcessing(depth);
+    // imageProcessing();
+
 
 }
 
@@ -120,7 +173,7 @@ int main(int argc, char** argv) {
 	ros::Rate loop_rate(20);
 	while (ros::ok()) {
 		ros::spinOnce();
-		imageProcessing();
+		// imageProcessing();
 		loop_rate.sleep();
 	}
 
